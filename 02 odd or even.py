6d353cc24730c5a5bcaf02970802e3bb5e8ffb7b
Tucker Campbell <tcampbell0092@gmail.com>
2019-06-15 10:40:58 -0500

    added 02 odd or even.py

---
+++ 02 odd or even.py
@@ -0,0 +1,8 @@
+number = int(input("Please enter a number: "))
+
+if number % 2 == 0:
+    print("The number you entered is even.")
+
+if number % 2 == 1:
+    print("The number you entered is odd.")
+

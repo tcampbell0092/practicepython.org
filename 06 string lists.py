900e42547ede76dee532a00727a438a0a5cc1c7c
Tucker Campbell <tcampbell0092@gmail.com>
2019-06-22 20:41:47 -0500

    Added 06 string lists.py

---
+++ 06 string lists.py
@@ -0,0 +1,9 @@
+pal = input("Please enter a string: ")
+pal_reversed = pal[::-1]
+
+if pal == pal_reversed:
+    print("The string is a palindrome")
+
+else:
+    print("The string is not a palindrome")
+

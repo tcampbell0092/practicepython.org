6657c16c5394701b43b735f6f0d733f7c3d6baea
Tucker Campbell <tcampbell0092@gmail.com>
2019-09-25 14:05:00 -0500

    Added comments to 28 max of three.py

--- 28 max of three.py
+++ 28 max of three.py
@@ -1,12 +1,16 @@
+# heapq.nlargest() returns list of n largest values
 import heapq
 
 
+# define function for blank list
 def max_three():
     num_list = []
 
+    # allows for 3 numbers to be entered in the list
     for i in range(0, 3):
         element = int(input("Enter a number: "))
 
+        #  prints out largest number in list
         num_list.append(element)
     print("The largest number in the list is ", (heapq.nlargest(1, num_list)))
 
